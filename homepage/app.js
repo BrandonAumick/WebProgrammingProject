b0efffe8f6336d7600163de9b479cccc2e46b5dd
Brandon Aumick <32585448+BrandonAumick@users.noreply.github.com>
2023-04-29 13:17:06 -0400

    Update app.js

--- homepage/app.js
+++ homepage/app.js
@@ -1,11 +1,48 @@
-document.getElementById("searchButton").addEventListener("click", function(event) {
-    checkForm();
-    // Prevent default form action.
-    event.preventDefault();
- });
+const playerSubmit = document.getElementById('playerSubmit');
+var nameText1 = document.getElementById('player1');
+var nameText2 = document.getElementById('player2');
 
- function checkForm() {
-    const name = document.getElementById('name_input1');
+
+playerSubmit.addEventListener('click', async () => {
+    
+   if (await checkForm()) {
+       return;
+   }
+
+    nameText1 = document.getElementById('player1');
+    nameText2 = document.getElementById('player2');
+    sessionStorage.setItem('name1', nameText1.value);
+    sessionStorage.setItem('name2', nameText2.value);
+
+    if (sessionStorage.getItem('leaderboard')) {
+
+        window.location.href = `http://localhost:3000/?player1=${nameText1.value}&player2=${nameText2.value}&leaderboard=${sessionStorage.getItem('leaderboard')}`;
+
+    } else {
+        
+        window.location.href = `http://localhost:3000/?player1=${nameText1.value}&player2=${nameText2.value}`;
+
+    }
+    
+
+    event.preventDefault();
+
+});
+
+
+window.onbeforeunload = () => {
+
+    if (window.location.href === 'http://localhost:3000/' || window.location.href === "http://localhost:3000") {
+        
+        sessionStorage.clear();
+
+    }
+
+}
+
+
+async function checkForm() {
+    const name = document.getElementById('player1');
     const error = document.getElementById('errorMessage');
     error.innerHTML = ''; // clear previous error messages
     let errorsFound = false;
@@ -21,25 +58,29 @@ document.getElementById("searchButton").addEventListener("click", function(event
     // Check for username length
     if (name.value.length < 1 || name.value.length > 26) {
         errorsFound = true;
-        password.classList.add('error');
-        const passErr1 = document.createElement('li');
-        passErr1.textContent = 'Username must be between 1 and 26 characters.';
-        error.appendChild(passErr1);
+        name.classList.add('error');
+        const nameErr1 = document.createElement('li');
+        nameErr1.textContent = 'Username must be between 1 and 26 characters.';
+        error.appendChild(nameErr1);
     }
   
     // Check for invalid username
     if (!name.value || !/^[a-zA-z0-9]*[a-zA-z][a-zA-z0-9]*$/.test(name.value)) {
       errorsFound = true;
-      email.classList.add('error');
-      const emailErr = document.createElement('li');
-      emailErr.textContent = 'Invalid Username.';
-      error.appendChild(emailErr);
+      name.classList.add('error');
+      const nameErr = document.createElement('li');
+      nameErr.textContent = 'Invalid Username.';
+      error.appendChild(nameErr);
     }
   
     if (errorsFound) {
-      error.classList.remove('hide');
+      error.hidden = false;
     } else {
-      error.classList.add('hide');
+      error.hidden = true;
       name.classList.remove('error');
     }
+
+
+    return errorsFound;
+
   }
